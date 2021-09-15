3693886915cd4fff3c0a2a63e104bfe44b1666ca
drpritomsaha <pritom1807@gmail.com>
2021-09-15 23:14:18 +0600

    add appBAr

--- lib/main.dart
+++ lib/main.dart
@@ -126,6 +126,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Log In"),
+      ),
       body: Stack(
         children: <Widget>[
           Container(
